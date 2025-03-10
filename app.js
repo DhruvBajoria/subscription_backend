8e84a7a67419e6d00bc7dafd9fbcdae87616484c
Dhruv Bajoria <dhruvbajoria2001@gmail.com>
2025-03-11 00:01:13 +0530

    db setup

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import { PORT } from './config/env.js';
-import userRouter from './routes/user.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
-import authRouter from './routes/auth.routes.js';
-
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/subscriptions',subscriptionRouter);
 
-app.get('/',(req, res) =>{
-    res.send('Hello World');
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/',(req, res) => {res.send('welcome to api resposnse for subscription management')});
+
+app.listen(PORT,async() => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+    await connectToDatabase();
 });
-
-app.listen(PORT, () =>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
 
 export default app;
