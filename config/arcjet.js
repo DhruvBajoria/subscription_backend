a7377a1e8a817e103ded367193d487d6139cd64c
Dhruv Bajoria <dhruvbajoria2001@gmail.com>
2025-03-14 23:26:00 +0530

    testing

--- config/arcjet.js
+++ config/arcjet.js
@@ -8,7 +8,9 @@ const aj = arcjet({
     shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
-      allow: [ "CATEGORY:SEARCH_ENGINE" ],
+      allow: [ "CATEGORY:SEARCH_ENGINE",
+         "HEADER:User-Agent=PostmanRuntime/*"
+       ],
     }),
     tokenBucket({
       mode: "LIVE",
